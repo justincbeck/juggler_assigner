ff3876d6eff507e01d2f278b1e4d62279aa7d990
Justin Beck <justinbeck@mac.com>
2011-08-09 16:18:31 -0400

    Finally, resolved max_size issue

--- lib/juggler_assigner/assigner.rb
+++ lib/juggler_assigner/assigner.rb
@@ -7,21 +7,15 @@ module JugglerAssigner
     def process(courses = nil, jugglers = nil)
       @revoked = Array.new
 
-      start = Time.new
-
       @max_team_size = jugglers.size / courses.size if @max_team_size.nil?
 
-      # For each juggler
       jugglers.each do |j|
         if !j.assigned
-          # First try to assign to a preferred course
           j.preferences.each do |c|
             assign_juggler(c, j) unless j.assigned
           end
 
-          # If I couldn't assign to a preferred course, assign to the best match
           if !j.assigned
-            # Sort the courses by their dot-product to the current juggler in descending order
             courses.sort! { |x, y| j.calculate_dot_product(y) <=> j.calculate_dot_product(x) }.each do |c|
               assign_juggler(c, j) unless j.assigned
             end
@@ -29,12 +23,8 @@ module JugglerAssigner
         end
       end
 
-      stop = Time.new
-      puts "Elapsed time for this run: " + (stop - start).to_s + " seconds"
-
       process(courses, @revoked) unless @revoked.size == 0
 
-      puts "All done!"
       print(courses)
 
     end
@@ -51,13 +41,10 @@ module JugglerAssigner
 
     def assign_juggler(course, juggler)
       if course.jugglers.size < @max_team_size
-        # If the course has room then go ahead and assign this juggler to it
         juggler.assigned = true
         juggler.assigned_course = course
 
         course.jugglers << juggler
-        puts "Inserting: " + juggler.name + " (" + juggler.calculate_dot_product(course).to_s + ") to " + course.name + " (" + course.jugglers.size.to_s + ")"
-        puts
       else
         force_assign_juggler(course, juggler)
       end
@@ -75,20 +62,9 @@ module JugglerAssigner
 
           @revoked << r
 
-          puts "Revoking: " + r.name + " (" + r.calculate_dot_product(course).to_s + ") from " + course.name + " (" + course.jugglers.size.to_s + ")"
-          revoked_s = "Revoked:"
-          @revoked.each do |i|
-            revoked_s += " "
-            revoked_s += i.name
-          end
-          puts revoked_s
-
           assign_juggler(course, juggler)
         end
         break if juggler.assigned
-      end
-      if !juggler.assigned
-        puts "Failed to assign: " + juggler.name + " to course: " + course.name + " (" + course.jugglers.size.to_s + ")"
       end
     end
 
