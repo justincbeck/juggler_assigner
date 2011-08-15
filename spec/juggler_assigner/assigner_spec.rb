5f2bfb574a031717139001f4fcf3f2352974d77d
Justin Beck <justinbeck@mac.com>
2011-08-15 09:12:56 -0400

    Refactoring

--- spec/juggler_assigner/assigner_spec.rb
+++ spec/juggler_assigner/assigner_spec.rb
@@ -6,8 +6,9 @@ module JugglerAssigner
       it "should assign all jugglers to courses" do
         courses = [ Course.new("C C0 H:5 E:8 P:7") ]
         jugglers = [ Juggler.new("J J0 H:6 E:7 P:4 C0", courses), Juggler.new("J J1 H:7 E:4 P:9 C0", courses) ]
-        assigner = Assigner.new("output.txt")
+        assigner = Assigner.new("tmp/output.txt")
         assigner.assign(courses, jugglers)
+        # TODO: What to test for?
       end
     end
   end
